933e2bb8e1b3db7ef90a73b78be796be72fa557d
nandolopez7 <hernando.rincon@correounivalle.edu.co>
2025-09-24 21:32:56 -0500

    New projects, API to insta screenshot add

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,36 +1,92 @@
-import { faHtml5, faCss3Alt, faReact, faJs, faBootstrap, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faHtml5,
+  faCss3Alt,
+  faReact,
+  faJs,
+  faBootstrap,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const projects = [
   {
     title: "Finding China",
-    description: "Discover the history of ancient China, with lots of multimedia content and all the knowledge about this culture!",
-    image: "/images/findingchina.webp",
+    description:
+      "Discover the history of ancient China, with lots of multimedia content and all the knowledge about this culture!",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://findingchina.netlify.app/",
     technologies: [faHtml5, faCss3Alt, faReact, faJs, faBootstrap],
     liveLink: "https://findingchina.netlify.app/",
-    repoLink: "https://github.com/NataliaLopezO/frontend_proyecto_integrador_china",
+    repoLink:
+      "https://github.com/NataliaLopezO/frontend_proyecto_integrador_china",
   },
   {
     title: "Portfolio",
     description: "A personal space designed to show you all my stuff!",
-    image: "/images/portfolio.webp",
-    technologies: [faHtml5, faCss3Alt, faBootstrap],
-    liveLink: "https://nandoportfolio.netlify.app/",
-    repoLink: "https://github.com/nandolopez7/Porfolio",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://portfoliohernando-v2.vercel.app/",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://portfoliohernando-v2.vercel.app/",
+    repoLink: "https://github.com/nandolopez7/PortfolioV2",
   },
   {
     title: "Shopping Cart",
-    description: "Website with a shopping cart where I was able to practuce useState and useContext",
-    image: "/images/shoppingCart.webp",
+    description:
+      "Website with a shopping cart where I was able to practuce useState and useContext",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://shoppingcarttest.netlify.app/",
     technologies: [faReact, faCss3Alt],
     liveLink: "https://shoppingcarttest.netlify.app/",
     repoLink: "https://github.com/nandolopez7/ShoppingCart",
   },
   {
     title: "ChessPizza",
-    description: "A website dedicated to PizzaLovers, where React concepts, html and css where applied",
-    image: "/images/Chess-Pizza.webp",
+    description:
+      "A website dedicated to PizzaLovers, where React concepts, html and css where applied",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://chesspizza.netlify.app/",
     technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
     liveLink: "https://chesspizza.netlify.app/",
     repoLink: "https://github.com/nandolopez7/ChessPizza",
+  },
+  {
+    title: "Errors in Gamification Design",
+    description:
+      "A website dedicated to dive deep into the errors that can be found in gamification design with a simple and modern design, and a mini-game at the end.",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://gamificacion-sand.vercel.app/",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://gamificacion-sand.vercel.app/",
+    repoLink: "https://github.com/nandolopez7/gamificacion",
+  },
+  {
+    title: "Tepary Beans",
+    description:
+      "A website dedicated to Tepary Beans, where React concepts, html and css where applied, also a TeparyChat bot was implemented and a Beans and seed calculator.",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://teparybeans.vercel.app/",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://teparybeans.vercel.app/",
+    repoLink: "https://github.com/nandolopez7/tepary_beans_deploy",
+  },
+  {
+    title: "TicketHub",
+    description:
+      "A website dedicated to sell/buy tickets for events, where React concepts, html and css where applied, also a payment gateway was implemented.",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://tickethub-front.vercel.app/",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://tickethub-front.vercel.app/",
+    repoLink: "https://github.com/nandolopez7/tickethub-front",
+  },
+  {
+    title: "BeautBuilder",
+    description:
+      "A website dedicated to create playlists with a modern and simple design, where React concepts, html and css where applied, using web scraping to get the songs.",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://beatbuilder.vercel.app/",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://beatbuilder.vercel.app/",
+    repoLink: "https://github.com/nandolopez7/beatbuilder",
+  },
+  {
+    title: "NutriApp",
+    description:
+      "A website dedicated to nutrition, where React concepts, html and css where applied, using an external API to get the nutritional information of the foods. A chatbot was also implemented. Login system with JWT and a payment simulation gateway was added.",
+    image: "https://api.apiflash.com/v1/urltoimage?access_key=eafe3d04d93f496e984a41a58399a8c6&wait_until=page_loaded&format=webp&url=https://nutriapp-1i6dt19ca-carolain3472s-projects.vercel.app/#que-es",
+    technologies: [faReact, faHtml5, faCss3Alt, faBootstrap],
+    liveLink: "https://nutriapp-1i6dt19ca-carolain3472s-projects.vercel.app/#que-es",
+    repoLink: "https://github.com/carolain3472/Nutriapp/tree/main",
   },
 ];
